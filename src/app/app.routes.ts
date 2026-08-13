8b8484fb3a66f673303c095ff1a46d932b80cb57
erisson <erisson.o@aluno.senai.br>
2026-08-13 16:54:31 -0300

    criado HomeComponetn, ajustes no css atleta, feito form no html atleta

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,22 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+import { AtletaComponent } from './component/atleta-component/atleta-component';
+
+import { HomeComponent } from './component/home-component/home-component';
+
+export const routes: Routes = [
+    {
+       path:"",
+       redirectTo:"/home",
+       pathMatch:"full"
+    },
+    {
+        path: "home",
+        Component:HomeComponent
+    },
+    {
+        path:"cadastroAtlteta",
+        Component: AtletaComponent
+    }
+
+];
