efd043a10dcab3720fb92de8342c570a52bcf3e6
erisson <erisson.o@aluno.senai.br>
2026-08-14 15:39:02 -0300

    feito remover, e alterando elemeno na array

--- src/app/service/atleta-service.ts
+++ src/app/service/atleta-service.ts
@@ -7,4 +7,30 @@ import { Injectable } from '@angular/core';
 export class AtletaService {
     //Declaração de arrays
     private atletas: Atlteta [] = []
-}
+
+    //declaração de funções de manipulação do array 
+    //adicionando Elemento
+    adicionarAtleta(atleta: Atleta){
+        this.atletas.push(atleta)
+    }
+
+    //listar elementos
+    listarAtletas(){
+        console.table(this.atletas)
+
+        return this.atletas
+    }
+
+    //remover elementos
+    removerElemento(idAtleta: number){
+        this.atletas = this.atletas.findIndex(elem=>elem.id |== atleta.id)
+        this.atletas.splice(1,posArray)
+
+        //alterando elemento do array
+        alterarelemento(atleta: Atleta){
+            let posArray = this.atletas.findIndex(elem=>elem.id |== atleta.id)
+            this.atletas[posArray]=atleta
+        }
+    }
+
+
