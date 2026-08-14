20cdb2a320c5300a66dbb52315d243329471e54a
erisson <erisson.o@aluno.senai.br>
2026-08-14 14:45:53 -0300

    adicionado service e arrays

--- src/app/component/atleta-component/atleta-component.ts
+++ src/app/component/atleta-component/atleta-component.ts
@@ -1,9 +1,40 @@
 import { Component } from '@angular/core';
-
+import {FormsModule} from '@angular/forms'
 @Component({
   selector: 'app-atleta-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './atleta-component.html',
   styleUrl: './atleta-component.css',
 })
-export class AtletaComponent {}
+export class AtletaComponent {
+  //DECLARANDO ATRIBUTOS
+  nome = 'Teste'
+  cpf = 0
+  sexo = ''
+  cep = 0
+  rua = ''
+  bairro = ''
+  cidade =''
+  uf =''
+
+  //declaração de funções
+  exibirDados(){
+    console.log(this.nome, this.cpf, this.sexo, this.cep, this.rua, this.bairro, this.cidade, this.uf   )
+    this.limparDados()
+  }
+
+  limparDados() {
+    this.nome= ''
+     this.cpf=0
+     this.sexo=''
+     this.cep=''
+     this.rua=''
+     this.bairro=''
+     this.cidade=''
+     this.uf=''
+  }
+ 
+}
+
+
+
---
+++ src/app/service/atleta-service.ts
@@ -0,0 +1,10 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+    providedIn: 'root'
+})
+
+export class AtletaService {
+    //Declaração de arrays
+    private atletas: Atlteta [] = []
+}
