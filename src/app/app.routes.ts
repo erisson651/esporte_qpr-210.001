720af508bc50700032e9617cc45a1dfc46ba0b04
erisson <erisson.o@aluno.senai.br>
2026-08-17 13:44:56 -0300

    feito o cadastro de corrida

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,27 +1,24 @@
 import { Routes } from '@angular/router';
 
-import { AtletaComponent } from './component/atleta-component/atleta-component';
-
 import { HomeComponent } from './component/home-component/home-component';
-
+import { AtletaComponent } from './component/atleta-component/atleta-component';
 import { CorridaComponent } from './component/corrida-component/corrida-component';
 
 export const routes: Routes = [
-    {
-       path:"",
-       redirectTo:"home",
-       pathMatch:"full"
-    },
-    {
-        path: "home",
-        component:HomeComponent
-    },
-    {
-        path:"cadastroAtleta",
-        component:AtletaComponent
-    },
-{
-    path:"corrida",
-    component:CorridaComponent
-}
+
+  {
+    path: '',
+    component: HomeComponent
+  },
+
+  {
+    path: 'cadastroAtleta',
+    component: AtletaComponent
+  },
+
+  {
+    path: 'cadastroCorrida',
+    component: CorridaComponent
+  }
+
 ];
--- src/app/component/corrida-component/corrida-component.ts
+++ src/app/component/corrida-component/corrida-component.ts
@@ -1,16 +1,48 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-corrida-component',
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './corrida-component.html',
-  styleUrl: './corrida-component.css',
+  styleUrl: './corrida-component.css'
 })
 export class CorridaComponent {
-  nome = ''
-  cpf = 0
-  categoria =''
-  distancia =''
-  cidade = ''
-  uf = ''
+
+  nome = '';
+  cpf = '';
+  categoria = '';
+  distancia = '';
+  cidade = '';
+  uf = '';
+
+  corridas: any[] = [];
+
+  salvar() {
+
+    const corrida = {
+      nome: this.nome,
+      cpf: this.cpf,
+      categoria: this.categoria,
+      distancia: this.distancia,
+      cidade: this.cidade,
+      uf: this.uf
+    };
+
+    this.corridas.push(corrida);
+
+    console.log('Corrida cadastrada:', corrida);
+
+    this.limpar();
+  }
+
+  limpar() {
+    this.nome = '';
+    this.cpf = '';
+    this.categoria = '';
+    this.distancia = '';
+    this.cidade = '';
+    this.uf = '';
+  }
+
 }
